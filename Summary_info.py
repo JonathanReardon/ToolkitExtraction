b0da2f2243383850230435a5c99294a4cd7615c9
JonathanReardon <jonathanjreardon@gmail.com>
2020-05-17 03:32:12 +0100

    added refactored extraction code for 6 vars. works

--- Summary_info.py
+++ Summary_info.py
@@ -37,12 +37,10 @@ for xs, ys in zip(x, y):
     df.append([xs, ys])
 
 # Variable option counts
-dict = {"Toolkit strand(s) (select at least one Toolkit strand)": 34,
-        "What was the level of assignment?": 7, 
+dict = {"What was the level of assignment?": 7, 
         "How were participants assigned?": 8,
         "How realistic was the study?": 4, 
         "What is the gender of the students?": 5,
-        "What is the age of the students? (Select ALL that apply)": 18, 
         "Section 1 What is the publication type?": 6,
         "What is the educational setting (Select ALL that apply)": 12,
         "In which country/countries was the study carried out? (Select ALL that apply)": 188}
@@ -60,5 +58,36 @@ def get_info():
 
 all = get_info()
 
-for dict in all:
-    pprint(dict)
+level_assignment_codes=all[0]
+participant_assignment_codes=all[1]
+study_realism_codes=all[2]
+student_gender_codes=all[3]
+pub_type_codes=all[4]
+edu_setting_codes=all[5]
+country_codes=all[6]
+
+codelist = [level_assignment_codes, participant_assignment_codes,
+            study_realism_codes, student_gender_codes,
+            pub_type_codes, edu_setting_codes, country_codes]
+
+def get_data():
+    all=[]
+    exclude="NA"
+    for var in range(len(codelist)):
+        holder=[]
+        for section in range(len(data["References"])):
+            if "Codes" in data["References"][section]:
+                holderfind, holdervalue = [], []
+                for study in range(len(data["References"][section]["Codes"])):
+                    for key, value in codelist[var].items():
+                        if key == data["References"][section]["Codes"][study]["AttributeId"]:
+                            holderfind, holdervalue = value, key
+                holder.append(holderfind)
+        all.append(holder)
+    return all
+
+data = get_data()
+for lst in data:
+    print(len(lst))
+
+
