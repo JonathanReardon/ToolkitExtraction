9cdbf91836401b1deab1c96f99616cb9f658acd2
JonathanReardon <jonathanjreardon@gmail.com>
2020-06-09 21:27:24 +0100

    added outcome type for each outcome / study

--- EffectSizeDetails.py
+++ EffectSizeDetails.py
@@ -87,6 +87,24 @@ def get_CIlowerSMD():
                 cilowerholder.append(exclude)
             CIlowerSMD.append(cilowerholder)
 
+# get Outcome (primary/secondary) info from 'Outcomes' section for all studies and outcomes
+def get_Outcome():
+    global Outcome
+    Outcome = []
+    for section in range(len(data["References"])):
+        outcomeholder = []
+        if "Outcomes" in data["References"][section]:
+            for subsection in range(10):
+                if subsection < len(data["References"][section]["Outcomes"]):
+                    outcomeholder.append(data["References"][section]["Outcomes"][subsection]["OutcomeText"])
+                else:
+                    outcomeholder.append(exclude)
+            Outcome.append(outcomeholder)
+        else:
+            for i in range(10):
+                outcomeholder.append(exclude)
+            Outcome.append(outcomeholder)
+
 def get_basic_info():
     global titles, itemids
     titles, itemids = [], []
@@ -105,6 +123,7 @@ get_SMD()
 get_SESMD()
 get_CIupperSMD()
 get_CIlowerSMD()
+get_Outcome()
 
 smd     = pd.DataFrame(SMD)
 sesmd   = pd.DataFrame(SESMD)
@@ -112,22 +131,28 @@ titles  = pd.DataFrame(titles)
 itemids = pd.DataFrame(itemids)
 ciupper = pd.DataFrame(CIupperSMD)
 cilower = pd.DataFrame(CIlowerSMD)
+outcome = pd.DataFrame(Outcome)
 
-df = pd.concat([itemids, titles, smd, sesmd, ciupper, cilower], axis=1, sort=False)
+df = pd.concat([itemids, titles, smd, sesmd, ciupper, cilower, outcome], axis=1, sort=False)
 
 df.columns = ['ItemID', 'Author', 
               'SMD_1', 'SMD_2', 'SMD_3', 'SMD_4', 'SMD_5', 'SMD_6', 'SMD_7', 'SMD_8', 'SMD_9', 'SMD_10',
               'SESMD_1', 'SESMD_2', 'SESMD_3', 'SESMD_4', 'SESMD_5', 'SESMD_6', 'SESMD_7', 'SESMD_8', 'SESMD_9', 'SESMD_10',
               'CIupperSMD_1', 'CIupperSMD_2', 'CIupperSMD_3', 'CIupperSMD_4', 'CIupperSMD_5', 'CIupperSMD_6', 'CIupperSMD_7', 'CIupperSMD_8', 'CIupperSMD_9', 'CIupperSMD_10',
-              'CIlowerSMD_1', 'CIlowerSMD_2', 'CIlowerSMD_3', 'CIlowerSMD_4', 'CIlowerSMD_5', 'CIlowerSMD_6', 'CIlowerSMD_7', 'CIlowerSMD_8', 'CIlowerSMD_9', 'CIlowerSMD_10']
+              'CIlowerSMD_1', 'CIlowerSMD_2', 'CIlowerSMD_3', 'CIlowerSMD_4', 'CIlowerSMD_5', 'CIlowerSMD_6', 'CIlowerSMD_7', 'CIlowerSMD_8', 'CIlowerSMD_9', 'CIlowerSMD_10',
+              'Outcome_1', 'Outcome_2', 'Outcome_3', 'Outcome_4', 'Outcome_5', 'Outcome_6', 'Outcome_7', 'Outcome_8', 'Outcome_9', 'Outcome_10']
 
 df = df[['ItemID', 'Author', 
-         'SMD_1', 'SESMD_1', 'CIupperSMD_1', 'CIlowerSMD_1', 'SMD_2', 'SESMD_2', 'CIupperSMD_2', 'CIlowerSMD_2',
-         'SMD_3', 'SESMD_3', 'CIupperSMD_3', 'CIlowerSMD_3', 'SMD_4', 'SESMD_4', 'CIupperSMD_4', 'CIlowerSMD_4',
-         'SMD_5', 'SESMD_5', 'CIupperSMD_5', 'CIlowerSMD_5', 'SMD_6', 'SESMD_6', 'CIupperSMD_6', 'CIlowerSMD_6',
-         'SMD_7', 'SESMD_7', 'CIupperSMD_7', 'CIlowerSMD_7', 'SMD_8', 'SESMD_8', 'CIupperSMD_8', 'CIlowerSMD_8',
-         'SMD_9', 'SESMD_9', 'CIupperSMD_9', 'CIlowerSMD_9', 'SMD_10', 'SESMD_10', 'CIupperSMD_10', 'CIlowerSMD_10']]
-
+         'SMD_1', 'SESMD_1', 'CIupperSMD_1', 'CIlowerSMD_1', 'Outcome_1', 
+         'SMD_2', 'SESMD_2', 'CIupperSMD_2', 'CIlowerSMD_2', 'Outcome_2', 
+         'SMD_3', 'SESMD_3', 'CIupperSMD_3', 'CIlowerSMD_3', 'Outcome_3', 
+         'SMD_4', 'SESMD_4', 'CIupperSMD_4', 'CIlowerSMD_4', 'Outcome_4', 
+         'SMD_5', 'SESMD_5', 'CIupperSMD_5', 'CIlowerSMD_5', 'Outcome_5', 
+         'SMD_6', 'SESMD_6', 'CIupperSMD_6', 'CIlowerSMD_6', 'Outcome_6', 
+         'SMD_7', 'SESMD_7', 'CIupperSMD_7', 'CIlowerSMD_7', 'Outcome_7', 
+         'SMD_8', 'SESMD_8', 'CIupperSMD_8', 'CIlowerSMD_8', 'Outcome_8', 
+         'SMD_9', 'SESMD_9', 'CIupperSMD_9', 'CIlowerSMD_9', 'Outcome_9', 
+         'SMD_10', 'SESMD_10', 'CIupperSMD_10', 'CIlowerSMD_10',  'Outcome_10']]
 
 df = df.applymap(lambda x: round(x, 4) if isinstance(x, (int, float)) else x)
 df.to_csv("EffectSizeDetails.csv", index=False)
