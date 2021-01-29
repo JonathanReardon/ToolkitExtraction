640952fd0a8da43315ddc0449d6a6fcd1c5a807e
jonathan reardon <jonathanjreardon@gmail.com>
2021-01-29 23:58:22 +0000

    import main function, auto save as appended file name

--- CustomExtractions/DataCleaning/SampleSize/DataFrame3.py
+++ CustomExtractions/DataCleaning/SampleSize/DataFrame3.py
@@ -1,3 +1,4 @@
+from Main import file
 from eppi_ID import eppiid_df
 from Author import author_df
 from Date import year_df
@@ -10,7 +11,6 @@ from ses_fsm import ses_fsm_df
 from Sample_Size_Initial import initial_sample_size_df
 from Sample_Size_Analyzed import analyzed_sample_size_df
 from Attrition import attrition_df
-
 import pandas as pd
 
 all_variables = pd.concat([
@@ -27,36 +27,40 @@ all_variables = pd.concat([
 ], axis=1, sort=False)
 
 # insert empty columns per variable for data checkers to log changes
-all_variables.insert(5, 'strand_CLEAN', '')
-all_variables.insert(8, 'sample_analysed_CLEAN', '')
-all_variables.insert(12, 'part_gen_CLEAN', '')
-all_variables.insert(15, 'fsm_prop_CLEAN', '')
-all_variables.insert(18, 'fsm_perc_CLEAN', '')
-all_variables.insert(21, 'fsm_info_CLEAN', '')
-all_variables.insert(24, 'fsm_na_CLEAN', '')
-all_variables.insert(27, 'base_n_treat_CLEAN', '')
-all_variables.insert(30, 'base_n_cont_CLEAN', '')
-all_variables.insert(33, 'base_n_treat2_CLEAN', '')
-all_variables.insert(36, 'base_n_treat3_CLEAN', '')
-all_variables.insert(39, 'n_treat_CLEAN', '')
-all_variables.insert(42, 'n_cont_CLEAN', '')
-all_variables.insert(45, 'n_treat2_CLEAN', '')
-all_variables.insert(48, 'n_cont2_CLEAN', '')
-all_variables.insert(52, 'attri_CLEAN', '')
-all_variables.insert(55, 'attri_treat_CLEAN', '')
-all_variables.insert(58, 'attri_perc_CLEAN', '')
+all_variables.insert(6, 'strand_CLEAN', '')
+all_variables.insert(9, 'sample_analysed_CLEAN', '')
+all_variables.insert(13, 'part_gen_CLEAN', '')
+all_variables.insert(16, 'fsm_prop_CLEAN', '')
+all_variables.insert(19, 'fsm_perc_CLEAN', '')
+all_variables.insert(22, 'fsm_info_CLEAN', '')
+all_variables.insert(25, 'fsm_na_CLEAN', '')
+all_variables.insert(28, 'base_n_treat_CLEAN', '')
+all_variables.insert(31, 'base_n_cont_CLEAN', '')
+all_variables.insert(34, 'base_n_treat2_CLEAN', '')
+all_variables.insert(37, 'base_n_treat3_CLEAN', '')
+all_variables.insert(40, 'n_treat_CLEAN', '')
+all_variables.insert(43, 'n_cont_CLEAN', '')
+all_variables.insert(46, 'n_treat2_CLEAN', '')
+all_variables.insert(49, 'n_cont2_CLEAN', '')
+all_variables.insert(53, 'attri_CLEAN', '')
+all_variables.insert(56, 'attri_treat_CLEAN', '')
+all_variables.insert(59, 'attri_perc_CLEAN', '')
 
 all_variables.replace('\r', ' ', regex=True, inplace=True)
 all_variables.replace('\n', ' ', regex=True, inplace=True)
 all_variables.replace(':', ' ',  regex=True, inplace=True)
 all_variables.replace(';', ' ',  regex=True, inplace=True)
 
-print(list(all_variables))
-
-''' del all_variables["MSR_Update 2020"] '''
+""" del all_variables["OL_Update_2020"] """
 
 for counter, i in enumerate(all_variables):
     print(counter, i)
 
-# save to disk
-all_variables.to_csv("SetS_14jan21_Sample_Size.csv", index=False)
+# get file name for output
+outfile_name = file.rsplit('/')[-1]
+outfile_name = outfile_name.rsplit('.')[0]
+outfile_name = outfile_name + "_Sample_Size.csv"
+
+# write to disk
+print("saving {}".format(outfile_name))
+all_variables.to_csv(outfile_name, index=False)
