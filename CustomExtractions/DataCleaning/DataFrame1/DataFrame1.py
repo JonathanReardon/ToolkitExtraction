b9275fb186321a9e02e17af232bdced5a052f61e
jonathan reardon <jonathanjreardon@gmail.com>
2021-01-29 23:54:10 +0000

    import main function file

--- CustomExtractions/DataCleaning/DataFrame1/DataFrame1.py
+++ CustomExtractions/DataCleaning/DataFrame1/DataFrame1.py
@@ -1,3 +1,4 @@
+from Main import file
 from eppi_ID import eppiid_df
 from Author import author_df
 from Date import year_df
@@ -17,7 +18,6 @@ from LevelofAssignment import level_of_assignment_df
 from StudyDesign import study_design_df
 from Randomisation import randomisation_df
 from Other_Outcomes import other_outcomes_df
-
 import pandas as pd
 
 all_variables = pd.concat([
@@ -43,29 +43,32 @@ all_variables = pd.concat([
 ], axis=1, sort=False)
 
 # insert empty columns per variable for data checkers to log changes
-all_variables.insert(6, 'strand_CLEAN', '')
-all_variables.insert(8, 'pub_eppi_CLEAN', '')
-all_variables.insert(12, 'pub_type_CLEAN', '')
-all_variables.insert(14, 'loc_country_CLEAN', '')
-all_variables.insert(18, 'int_Setting_CLEAN', '')
-all_variables.insert(22, 'eco_valid_CLEAN', '')
-all_variables.insert(26, 'part_age_CLEAN', '')
+all_variables.insert(7,  'strand_CLEAN', '')
+all_variables.insert(9,  'pub_eppi_CLEAN', '')
+all_variables.insert(13, 'pub_type_CLEAN', '')
+all_variables.insert(15, 'loc_country_CLEAN', '')
+all_variables.insert(19, 'int_Setting_CLEAN', '')
+all_variables.insert(23, 'eco_valid_CLEAN', '')
+all_variables.insert(27, 'part_age_CLEAN', '')
 
-all_variables.insert(29, 'school_treat_CLEAN', '')
-all_variables.insert(32, 'school_cont_CLEAN', '')
-all_variables.insert(35, 'school_total_CLEAN', '')
-all_variables.insert(39, 'school_na_CLEAN', '')
+all_variables.insert(30, 'school_treat_CLEAN', '')
+all_variables.insert(33, 'school_cont_CLEAN', '')
+all_variables.insert(36, 'school_total_CLEAN', '')
+all_variables.insert(40, 'school_na_CLEAN', '')
 
-all_variables.insert(42, 'class_treat_CLEAN', '')
-all_variables.insert(45, 'class_cont_CLEAN', '')
-all_variables.insert(48, 'class_total_CLEAN', '')
-all_variables.insert(52, 'class_na_CLEAN', '')
+all_variables.insert(43, 'class_treat_CLEAN', '')
+all_variables.insert(46, 'class_cont_CLEAN',  '')
+all_variables.insert(49, 'class_total_CLEAN', '')
+all_variables.insert(53, 'class_na_CLEAN', '')
 
-all_variables.insert(56, 'treat_group_CLEAN', '')
-all_variables.insert(60, 'part_assig_CLEAN', '')
-all_variables.insert(64, 'level_assig_CLEAN', '')
-all_variables.insert(68, 'int_design_CLEAN', '')
-all_variables.insert(72, 'rand_CLEAN', '')
+all_variables.insert(57, 'treat_group_CLEAN', '')
+all_variables.insert(61, 'part_assig_CLEAN', '')
+all_variables.insert(65, 'level_assig_CLEAN', '')
+all_variables.insert(69, 'int_design_CLEAN', '')
+all_variables.insert(73, 'rand_CLEAN', '')
+all_variables.insert(77, 'out_other_CLEAN', '')
+all_variables.insert(81, 'out_info_CLEAN', '')
+all_variables.insert(84, 'part_other_CLEAN', '')
 
 # remove problematic text from outputs
 all_variables.replace('\r', ' ', regex=True, inplace=True)
@@ -76,13 +79,18 @@ all_variables.replace(';', ' ',  regex=True, inplace=True)
 print(list(all_variables))
 
 # temporary whilst 'Update' strands have not been integrated into the main section
-# most files will not have this
-del all_variables["MSR_Update 2020"]
-
-# write to disk
-all_variables.to_csv("OL_14jan21_DataFrame1.csv", index=False)
+''' del all_variables["MSR_Update 2020"] '''
 
 # useful
 print("Columns:", all_variables.shape[1])
 print("Rows:", all_variables.shape[0])
 print("Datapoints:", all_variables.shape[0] * all_variables.shape[1])
+
+# get file name for output
+outfile_name = file.rsplit('/')[-1]
+outfile_name = outfile_name.rsplit('.')[0]
+outfile_name = outfile_name + "_DataFrame1.csv"
+
+# write to disk
+print("saving {}".format(outfile_name))
+all_variables.to_csv(outfile_name, index=False)
