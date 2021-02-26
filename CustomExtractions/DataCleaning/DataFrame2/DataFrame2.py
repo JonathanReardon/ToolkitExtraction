3cfb551c6cda3ef2fe10c75af3ba30191cf4e780
jonathan reardon <jonathanjreardon@gmail.com>
2021-02-26 22:04:34 +0000

    add mkdir part

--- CustomExtractions/DataCleaning/DataFrame2/DataFrame2.py
+++ CustomExtractions/DataCleaning/DataFrame2/DataFrame2.py
@@ -2,30 +2,26 @@ from Main import file
 from eppi_ID import eppiid_df
 from Author import author_df
 from Date import year_df
+from Abstract import abstract_df
 from AdminStrand import admin_strand_df
-from InterventionName import intervention_name_df
-from InterventionDescription import intervention_description_df
-from InterventionObjectives import intervention_objectives_df
-from InterventionOrganizationType import intervention_org_type
-from InterventionTrainingProvided import intervention_training_provided_df
-from InterventionFocus import intervention_focus_df
-from InterventionTeachingApproach import intervention_teaching_approach_df
-from InterventionInclusion import intervention_inclusion_df
-from InterventionTime import intervention_time_df
-from InterventionDelivery import intervention_delivery_df
-from InterventionDuration import intervention_duration_df
-from InterventionFrequency import intervention_frequency_df
-from InterventionSessionLength import intervention_session_length_df
-from InterventionDetail import intervention_detail_df
-from InterventionCostsReported import intervention_costs_df
-from InterventionEvaluation import intervention_evaluation_df  # includes EEF evaluation
-from Baseline_Differences import baseline_differences_df
-from Comparability import comparability_df
-from Comparability_Variables_Reported import comparability_vars_reported_df
-from Clustering import clustering_df
+from PublicationType_EPPI import pubtype_eppi_df
+from PublicationType import publication_type_df
+from Country import country_df
+from EducationalSetting import educational_setting_df
+from StudyRealism import study_realism_df
+from Age import student_age
+from NumberofSchools import number_of_schools_df
+from NumberofClasses import number_of_classes_df
+from TreatmentGroup import treatment_group_df
+from ParticipantAssignment import participant_assignment_df
+from LevelofAssignment import level_of_assignment_df
+from StudyDesign import study_design_df
+from Randomisation import randomisation_df
+from Other_Outcomes import other_outcomes_df
 
 import os
 import pandas as pd
+
 
 def make_dataframe(save_file=True, clean_cols=True, verbose=True):
 
@@ -33,54 +29,54 @@ def make_dataframe(save_file=True, clean_cols=True, verbose=True):
         eppiid_df,
         author_df,
         year_df,
+        abstract_df,
         admin_strand_df,
-        intervention_name_df,
-        intervention_description_df,
-        intervention_objectives_df,
-        intervention_org_type,
-        intervention_training_provided_df,
-        intervention_focus_df,
-        intervention_teaching_approach_df,
-        intervention_inclusion_df,
-        intervention_time_df,
-        intervention_delivery_df,
-        intervention_duration_df,
-        intervention_frequency_df,
-        intervention_session_length_df,
-        intervention_detail_df,
-        intervention_costs_df,
-        intervention_evaluation_df,
-        baseline_differences_df,
-        comparability_df,
-        comparability_vars_reported_df,
-        clustering_df
+        pubtype_eppi_df,
+        publication_type_df,
+        country_df,
+        educational_setting_df,
+        study_realism_df,
+        student_age,
+        number_of_schools_df,
+        number_of_classes_df,
+        treatment_group_df,
+        participant_assignment_df,
+        level_of_assignment_df,
+        study_design_df,
+        randomisation_df,
+        other_outcomes_df
     ], axis=1, sort=False)
 
-    # insert empty columns per variable for data checkers to log changes
-    all_variables.insert(5,  'strand_CLEAN',        '')
-    all_variables.insert(8,  'int_name_CLEAN',      '')
-    all_variables.insert(11, 'int_desc_CLEAN',      '')
-    all_variables.insert(14, 'int_object_CLEAN',    '')
-    all_variables.insert(15, 'int_prov_CLEAN',      '')
-    all_variables.insert(22, 'int_training_CLEAN',  '')
-    all_variables.insert(26, 'int_part_CLEAN',      '')
-    all_variables.insert(30, 'int_approach_CLEAN',  '')
-    all_variables.insert(34, 'digital_tech_CLEAN',  '')
-    all_variables.insert(38, 'parent_partic_CLEAN', '')
-    all_variables.insert(42, 'int_when_CLEAN',      '')
-    all_variables.insert(45, 'int_who_CLEAN',       '')
-    all_variables.insert(49, 'int_dur_CLEAN',       '')
-    all_variables.insert(52, 'int_freq_CLEAN',      '')
-    all_variables.insert(54, 'int_leng_CLEAN',      '')
-    all_variables.insert(59, 'int_fidel_CLEAN',     '')
-    all_variables.insert(63, 'int_cost_CLEAN',      '')
-    all_variables.insert(67, 'out_eval_CLEAN',      '')
-    all_variables.insert(69, 'eef_eval_CLEAN',      '')
-    all_variables.insert(73, 'base_diff_CLEAN',     '')
-    all_variables.insert(77, 'comp_anal_CLEAN',     '')
-    all_variables.insert(81, 'comp_var_rep_CLEAN',  '')
-    all_variables.insert(85, 'comp_var_CLEAN',      '')
-    all_variables.insert(89, 'clust_anal_CLEAN',    '')
+    if clean_cols:
+        # insert empty columns per variable for data checkers to log changes
+        all_variables.insert(6,  'strand_CLEAN', '')
+        all_variables.insert(9,  'pub_eppi_CLEAN', '')
+        all_variables.insert(12, 'pub_type_CLEAN', '')
+        all_variables.insert(14, 'loc_country_CLEAN', '')
+        all_variables.insert(18, 'int_Setting_CLEAN', '')
+        all_variables.insert(22, 'eco_valid_CLEAN', '')
+        all_variables.insert(26, 'part_age_CLEAN', '')
+
+        # school cols
+        all_variables.insert(29, 'school_treat_CLEAN', '')
+        all_variables.insert(32, 'school_cont_CLEAN', '')
+        all_variables.insert(35, 'school_total_CLEAN', '')
+        all_variables.insert(30, 'school_na_CLEAN', '')
+
+        # class cols
+        all_variables.insert(42, 'class_treat_CLEAN', '')
+        all_variables.insert(44, 'class_cont_CLEAN',  '')
+        all_variables.insert(50, 'class_total_CLEAN', '')
+        all_variables.insert(52, 'class_na_CLEAN', '')
+
+        all_variables.insert(56, 'treat_group_CLEAN', '')
+        all_variables.insert(60, 'part_assig_CLEAN', '')
+        all_variables.insert(64, 'level_assig_CLEAN', '')
+        all_variables.insert(68, 'int_design_CLEAN', '')
+        all_variables.insert(72, 'rand_CLEAN', '')
+        all_variables.insert(76, 'out_other_CLEAN', '')
+        all_variables.insert(80, 'out_info_CLEAN', '')
+        all_variables.insert(83, 'part_other_CLEAN', '')
 
     # remove problematic text from outputs
     all_variables.replace('\r', ' ', regex=True, inplace=True)
@@ -99,9 +95,10 @@ def make_dataframe(save_file=True, clean_cols=True, verbose=True):
         print("\n")
 
         # print dataframe info
-        print("Columns:", all_variables.shape[1])
-        print("Rows:", all_variables.shape[0])
-        print("Datapoints:", all_variables.shape[0] * all_variables.shape[1])
+        print("Columns: {}".format(all_variables.shape[1]))
+        print("Rows: {}".format(all_variables.shape[0]))
+        print("Datapoints: {}".format(
+            all_variables.shape[0] * all_variables.shape[1]))
         print("\n")
 
     if save_file:
@@ -112,12 +109,22 @@ def make_dataframe(save_file=True, clean_cols=True, verbose=True):
         # get file name for output
         outfile_name_pre = file.rsplit('/')[-1]
         outfile_name_mid = outfile_name_pre.rsplit('.')[0]  # use for dir name
-        outfile_name = outfile_name_mid + "_DataFrame2_compare.csv"
+        outfile_name = outfile_name_mid + "_DataFrame1.csv"
         outfile = os.path.join(cw + "/" + outfile_name_mid, outfile_name)
+
+        # create dir (filename)
+        try:
+            os.mkdir(outfile_name_mid)
+        except OSError:
+            print("Create {} dir fail, check if it already exists or permissions".format(
+                outfile_name_mid))
+        else:
+            print("Successfully created {} directory".format(outfile_name_mid))
 
         # write to disk
         print("Input file: {}".format(file))
         print("Saving extracted output to: {}".format(outfile))
         all_variables.to_csv(outfile, index=False)
+
 
 make_dataframe(save_file=True, clean_cols=True, verbose=True)
