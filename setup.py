363018ee0b9778455aa3d38ba6be247fd43e5ae1
JonathanReardon <jonathanjreardon@gmail.com>
2023-07-19 01:30:18 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 # Read the contents of your README file
-with open('README_pypi.md', 'r', encoding='utf-8') as fh:
+with open('README_package.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
     name='eef-data',
-    version='0.47',  # Remember to increment the version number
+    version='0.52',  # Remember to increment the version number
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -16,7 +16,9 @@ setup(
         'rich==12.4.4',
         'toolz==0.11.2'
     ],
+    python_requires='>=3.10.6',
     long_description=long_description,
+    package_data={'': ['README_package.md']},
     long_description_content_type='text/markdown',
     entry_points={
         'console_scripts': [
